c4f2056c2daf953a8a493a39375487668278978d
xbingham <xbingham@uncc.edu>
2022-04-13 19:44:03 -0400

    Task 6 - Save to file update.

--- ouline.py
+++ ouline.py
@@ -1,12 +1,15 @@
 #import beautifulsoup and request here
 from bs4 import BeautifulSoup;
 import requests;
+import json;
 
 def displayJobDetails(jobResult):
-    print(1)
     #data output
-    #print("====JOB FOUND====")
-    #print("Title: %s\nCompany: %s\nSalary: %s\nDescription:\n%s" % (title, company, salary, description))
+    print("====JOB FOUND====")
+    print("Title: %s\nCompany: %s\nSalary: %s\nDescription:" % (jobResult['title'], jobResult['company'], jobResult['salary']))
+    for subDesc in jobResult['description']:
+        print("\t-%s" % subDesc)
+    print("\n")
 
 #function to get job list from url 'https://www.indeed.com/jobs?q={role}&l={location}'
 def getJobList(role,location):
@@ -29,14 +32,14 @@ def getJobList(role,location):
         title = titleWrapper.text if (titleWrapper != None) else "N/A"
         company = companyWrapper.text if (companyWrapper != None) else "N/A"
         salary = salaryWrapper.text if (salaryWrapper != None) else "N/A"
-        description = ""
+        description = []
 
         #data cleanup
         if title[:3] == "new":
             title = title[3:]
 
         for subDescription in dirtiedDescriptionWrapper:
-            description += ("\t-" + subDescription.text + "\n")
+            description.append(subDescription.text)
 
         #data packing
         job = {
@@ -52,8 +55,10 @@ def getJobList(role,location):
 
 #save data in JSON file
 def saveDataInJSON(jobDetails):
-    #Complete the missing part of this function here
+    outFile = open('jobDetails.json', 'w')
+    json.dump(jobDetails, outFile)
     print("Saving data to JSON")
+    outFile.close()
 
 #main function
 def main():
@@ -66,7 +71,9 @@ def main():
     print("\n")
     
     jobList = getJobList(role, location);
-    print(jobList)
+    saveDataInJSON(jobList);
+    for job in jobList:
+        displayJobDetails(job);
     
 if __name__ == '__main__':
     main()
