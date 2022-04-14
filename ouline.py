cbd48435f8099e053b4a0a6d8d4f0501667c25e6
TomSakowski <98927378+TomSakowski@users.noreply.github.com>
2022-04-13 21:43:33 -0400

    Update ouline.py

--- ouline.py
+++ ouline.py
@@ -63,7 +63,12 @@ def saveDataInJSON(jobDetails):
 #main function
 def main():
     # Write a code here to get job location and role from user e.g. role = input()
-
+ print("Enter role you want to search")
+    role = input()
+    print("Enter location you want to search")
+    location = input()
+    print('Role: %s, Location: %s' % (role, location))
+    print("\n")
     
     jobList = getJobList(role, location);
     saveDataInJSON(jobList);
